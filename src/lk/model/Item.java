f1b36fbe3bdde7bb23e346fdcd0768c1409499f7
Ishara09 <isharapersonal97@gmail.com>
2021-08-07 13:33:25 +0530

    Item files added

---
+++ src/lk/model/Item.java
@@ -0,0 +1,54 @@
+/*
+ * *
+ *  * @author : Ishara Dinusanka <isharapesonal@gmail.com>
+ *  * @since : 08/07/2021
+ *  *
+ */
+
+package lk;
+
+public class Item {
+    private String name;
+    private String price;
+    private Date expDate;
+    private String qty;
+
+    public Item(String name, String price, String expDate, String qty) {
+        this.name = name;
+        this.price = price;
+        this.expDate = expDate;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public void setQty(String qty) {
+        this.qty = qty;
+    }
+}
